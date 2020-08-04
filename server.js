d2897021b4d5161a71488d4f36b3974ea1103b6a
Akash Chaudhary <akashfzd2000@gmail.com>
2020-08-05 00:54:37 +0530

    Routes created

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 connectDB();
 
 app.get('/', (req, res) => res.send('api running'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started at ${PORT}`));
