8b514462c7e3d4a25c0838339a055f3533c9aad2
Akash Chaudhary <akashfzd2000@gmail.com>
2020-08-05 00:27:49 +0530

    Express server created and database connected

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const { connect } = require('mongoose');
 
 const app = express();
+// connect database
+connectDB();
+
 app.get('/', (req, res) => res.send('api running'));
 const PORT = process.env.PORT || 5000;
 
