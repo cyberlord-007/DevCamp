3733d0cc10536422fede5ef7d7288a55d5a4b226
Akash Chaudhary <akashfzd2000@gmail.com>
2020-08-06 01:33:29 +0530

    Defined user model,mongoose schema and added express validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
-// @route  GET api/users
-// @desc   Test Route
+const { check, validationResult } = require('express-validator');
+// @route  Post api/users
+// @desc   Register Route
 // @access Public
 
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty,
+    check('email', 'Please enter a valid email').isEmail,
+    check(
+      'password',
+      'Your password must contain atleast 8 characters'
+    ).isLength({ min: 8 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const app = express();
 // connect database
 connectDB();
 
+// initializing middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('api running'));
+
+// defining routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
