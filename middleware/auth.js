c340b13d41323372b97dba28933f6b2cf0d70813
Akash Chaudhary <akashfzd2000@gmail.com>
2020-08-07 21:36:59 +0530

    Created My profile route

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,13 +4,13 @@ const config = require('config');
 module.exports = function (req, res, next) {
   const token = req.header('x-auth-token');
   if (!token) {
-    res.status(401).json({ msg: 'Unauthorized Access' });
+    return res.status(401).json({ msg: 'Unauthorized Access' });
   }
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
     req.user = decoded.user;
     next();
   } catch (err) {
-    response.status(401).json({ msg: 'Invalid token' });
+    res.status(500).json({ msg: 'Server Error' });
   }
 };
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
-
-// @route  GET api/profile
-// @desc   Test Route
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+// @route  GET api/profile/me
+// @desc   Current Profile Route
 // @access Public
 
-router.get('/', (req, res) => res.send('Profile route'));
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({ msg: 'User does not exists' });
+    }
+    res.json(profile);
+  } catch (err) {
+    res.status(500).json({ msg: 'Server Error!!' });
+  }
+});
 
 module.exports = router;
