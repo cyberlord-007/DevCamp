b6d00798388f12dace277956a6395ea9f768a338
Akash Chaudhary <akashfzd2000@gmail.com>
2020-08-09 04:29:09 +0530

    Delete comment route added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -12,7 +12,7 @@ const Profile = require('../../models/Profile');
 
 router.post(
   '/',
-  [auth, [check('text', 'Comment text is required').not().isEmpty()]],
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -78,12 +78,130 @@ router.get('/:id', auth, async (req, res) => {
 router.delete('/:id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-
+    // authenticate the user
     if (post.user.toString() !== req.user.id) {
       res.status(401).json({ msg: 'Unauthorized user' });
     }
     await post.remove();
     res.json({ msg: 'Post deleted successfully' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Server Error' });
+  }
+});
+
+// @route  PUT api/posts/like/:id
+// @desc   Like a post
+// @access Private
+
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    // check if it's already liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Server Error' });
+  }
+});
+
+// @route  PUT api/posts/unlike/:id
+// @desc   Unike a post
+// @access Private
+
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    // check if it's been liked yet
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'Post has not been liked yet' });
+    }
+    //   get unlike index
+
+    const unlikeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+    post.likes.splice(unlikeIndex, 1);
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Server Error' });
+  }
+});
+
+// @route  POST api/posts/comments/:id
+// @desc   Comment on a post
+// @access Private
+
+router.post(
+  '/comments/:id',
+  [auth, [check('text', 'Comment text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id);
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+      await post.save();
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: 'Server Error' });
+    }
+  }
+);
+
+// @route  POST api/posts/comments/:id
+// @desc   Comment on a post
+// @access Private
+
+router.delete('/comments/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+    if (!comment) {
+      return res.status(404).json({ msg: 'No comments' });
+    }
+
+    // check user
+    if (comment.user.toString() !== req.user.id) {
+      res.status(401).json({ msg: 'Unauthorized user' });
+    }
+
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
   } catch (err) {
     console.error(err.message);
     res.status(500).json({ msg: 'Server Error' });
