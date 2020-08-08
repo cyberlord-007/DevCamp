37ef9dfe63ed5fd11b7cd8357db5aabe435bf609
Akash Chaudhary <akashfzd2000@gmail.com>
2020-08-09 02:56:26 +0530

    Created post model & updated post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const Post = require('../../models/Posts');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
 
-// @route  GET api/posts
-// @desc   Test Route
-// @access Public
+// @route  POST api/posts
+// @desc   Create a post
+// @access Private
 
-router.get('/', (req, res) => res.send('Posts route'));
+router.post(
+  '/',
+  [auth, [check('text', 'Comment text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id);
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+      const post = await newPost.save();
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: 'Server Error' });
+    }
+  }
+);
 
 module.exports = router;
