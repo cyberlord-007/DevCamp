a4bc5d62e847806640dd5ebcf098f7c03f5944ce
Akash Chaudhary <akashfzd2000@gmail.com>
2020-08-08 23:28:42 +0530

    Experience route created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -154,4 +154,55 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+// @route  PUT api/profile/experience
+// @desc   Update user experience
+// @access Private
+
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newExperience = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExperience);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: 'Server Error' });
+    }
+  }
+);
+
 module.exports = router;
