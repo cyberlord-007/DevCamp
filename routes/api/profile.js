866a6dda120496c3b83dc788206e96a187755b15
Akash Chaudhary <akashfzd2000@gmail.com>
2020-08-09 01:23:13 +0530

    Added Fetch repos route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const request = require('request');
+const config = require('config');
 const Profile = require('../../models/Profile');
 const { check, validationResult } = require('express-validator');
 
@@ -295,6 +297,37 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 
     await profile.save();
     res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Server Error' });
+  }
+});
+
+// @route  GET api/profile/github/:username
+// @desc   Fetch github repos
+// @access Public
+
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('clientSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        res.status(404).json({ msg: 'No github profile found for this user.' });
+      }
+
+      res.json(JSON.parse(body));
+    });
   } catch (err) {
     console.error(err.message);
     res.status(500).json({ msg: 'Server Error' });
